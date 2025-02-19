e444a2d3e0645c96072fe5975d6ebe2e0a0ae320
david-alejo <churr.te@gmail.com>
2025-02-19 07:55:17 +0100

    Debugging docker execution Windows

---
+++ docker/run_container_windows.ps1
@@ -0,0 +1,10 @@
+docker run -it `
+--env="DISPLAY=host.docker.internal:0" `
+--name rssa `
+--net=host `
+--privileged `
+--mount type=bind,source=$HOME\rssa_shared,target=/home/rssa `
+rssa `
+bash
+    
+docker rm rssa

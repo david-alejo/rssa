ed35d1887105ffdba2352229bf54b2981a5f95ed
david-alejo <churr.te@gmail.com>
2026-04-06 17:23:32 +0200

    First semi-tested version

--- practica_3/scripts/path_publisher.py
+++ practica_3/scripts/path_publisher.py
@@ -8,37 +8,45 @@ from rclpy.node import Node
 from rclpy.parameter import Parameter
 from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Path
+import yaml
 
 class PathPublisher(Node):
     def __init__(self):
         super().__init__('path_publisher')
         self.publisher = self.create_publisher(Path, '/path', 10)
-        self.seq = 0
-        self.goals = sorted(self.get_parameter('path').value.items())
+        
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('path_file', rclpy.Parameter.Type.STRING),
+                ('frame', rclpy.Parameter.Type.STRING),
+            ]   
+        )
+        
+        with open(self.get_parameter('path_file').value, 'r') as f:
+            path_data = yaml.safe_load(f)
+        
+        self.goals = path_data['path']
         self.timer = self.create_timer(1.0, self.publish_path)
 
     def publish_path(self):
         path = Path()
         path.header.frame_id = self.get_parameter('frame').value or "map"
         path.header.stamp = self.get_clock().now().to_msg()
-        path.header.seq = self.seq
-        seq_goals = 0
         
         for i in self.goals:
             pose = PoseStamped()
             pose.header.frame_id = path.header.frame_id
-            pose.header.seq = seq_goals
-            seq_goals += 1
-            pose.pose.position.x = i[1]['x']
-            pose.pose.position.y = i[1]['y']
-            pose.pose.position.z = 0
+            print(i)
+            pose.pose.position.x = self.goals[i]['x'] 
+            pose.pose.position.y = self.goals[i]['x'] 
+            pose.pose.position.z = 0.0
             
             path.poses.append(pose)
             self.get_logger().info(f"Added point {pose.pose.position.x}, {pose.pose.position.y}")
         
         self.publisher.publish(path)
         self.get_logger().info("Path published!")
-        self.seq += 1
 
 def main(args=None):
     rclpy.init(args=args)
